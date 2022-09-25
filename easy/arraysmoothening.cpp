ff15b5f3eccf3186ad1a6001289f41c685b17437
Dom Binks <dominic.binks@gmail.com>
2022-09-26 00:28:39 +0100

    Slightly optimise Array Smoothening

--- easy/arraysmoothening.cpp
+++ easy/arraysmoothening.cpp
@@ -8,7 +8,6 @@ int main()
     int N, K;
     cin >> N >> K;
     map<int, int> occurrences;
-    int output;
 
     for(int i = 0; i < N; i++)
     {
@@ -17,22 +16,35 @@ int main()
         occurrences[num]++;
     }
 
+    int max;
+    vector<int> maxes;
     while(K >= 0)
     {
-        int max = 0;
+        max = 0;
+        maxes.clear();
         for(map<int, int>::iterator j = occurrences.begin(); j != occurrences.end(); j++)
         {
-            if(j->second > occurrences[max])
-                max = j->first;
+            if(j->second > max)
+            {
+                max = j->second;
+                maxes.clear();
+                maxes.push_back(j->first);
+            }
+            else if(j->second == max)
+                maxes.push_back(j->first);
         }
 
-        if(K > 0)
-            occurrences[max]--;
-        else
-            cout << occurrences[max] << endl;
+        if(K >= maxes.size())
+        {
+            for(vector<int>::iterator k = maxes.begin(); k != maxes.end(); k++)
+            {
+                occurrences[*k]--;
+            }
+        }
 
-        K--;
+        K -= maxes.size();
     }
 
+    cout << max << endl;
     return 0;
 }
