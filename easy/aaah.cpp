3b4e17cb51b54e8a84f78d162fbeb14894e15cc1
Dom Binks <dominic.binks@gmail.com>
2023-07-05 22:41:43 +0100

    Fix files to use spaces not tabs

--- easy/aaah.cpp
+++ easy/aaah.cpp
@@ -5,11 +5,11 @@ using namespace std;
 
 int main()
 {
-	// Get inputs
-	string one, two;
-	cin >> one >> two;
-	// Check to see if the first is longer than the second
-	one.size() >= two.size() ? cout << "go" << endl : cout << "no" << endl;
+    // Get inputs
+    string one, two;
+    cin >> one >> two;
+    // Check to see if the first is longer than the second
+    one.size() >= two.size() ? cout << "go" << endl : cout << "no" << endl;
 
-	return 0;
+    return 0;
 }
--- easy/harshadnumbers.cpp
+++ easy/harshadnumbers.cpp
@@ -4,30 +4,30 @@ using namespace std;
 
 int main()
 {
-	// Get input
-	int n;
-	cin >> n;
+    // Get input
+    int n;
+    cin >> n;
 
-	// While i >= 0 (true)
-	for (int i = 0; i >= 0 ; i++)
-	{
-		int divisor = 0; // Sum of digits
-		int m = n + i; // Number whose digits are being summed
+    // While i >= 0 (true)
+    for (int i = 0; i >= 0 ; i++)
+    {
+        int divisor = 0; // Sum of digits
+        int m = n + i; // Number whose digits are being summed
 
-		while (m > 0) // While there are digits left
-		{
-			divisor += (m % 10); // Add the right most digit to the divisor
-			m = m / 10; // Remove the rightmost digit
-		}
+        while (m > 0) // While there are digits left
+        {
+            divisor += (m % 10); // Add the right most digit to the divisor
+            m = m / 10; // Remove the rightmost digit
+        }
 
-		// If the number is a harshad number
-		if ((n + i) % divisor == 0)
-		{
-			// Output the number
-			cout << n + i << endl;
-			break; // End
-		}
-	}
+        // If the number is a harshad number
+        if ((n + i) % divisor == 0)
+        {
+            // Output the number
+            cout << n + i << endl;
+            break; // End
+        }
+    }
 
-	return 0;
+    return 0;
 }
