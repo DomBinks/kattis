fce041e2dc3e45bad48a3e408d18025f041be0f5
Dom Binks <dominic.binks@gmail.com>
2023-07-05 22:38:41 +0100

    Fix Fizz Buzz
    Fix Fizz Buzz by switching tabs to spaces

--- easy/fizzbuzz.cpp
+++ easy/fizzbuzz.cpp
@@ -5,32 +5,32 @@ using namespace std;
 
 int main()
 {
-	int x, y, n;
-	cin >> x >> y >> n;
+    int x, y, n;
+    cin >> x >> y >> n;
 
-	for(int i = 1; i <= n; i++)
-	{
-		string num = "";
+    for(int i = 1; i <= n; i++)
+    {
+        string num = "";
 
-		if(i % x == 0)
-		{
-			num.append("Fizz");
-		}
+        if(i % x == 0)
+        {
+            num.append("Fizz");
+        }
 
-		if(i % y == 0)
-		{
-			num.append("Buzz");
-		}
+        if(i % y == 0)
+        {
+            num.append("Buzz");
+        }
 
-		if(num == "")
-		{
-			cout << i << endl;
-		}
-		else
-		{
-			cout << num << endl;
-		}
-	}
+        if(num == "")
+        {
+            cout << i << endl;
+        }
+        else
+        {
+            cout << num << endl;
+        }
+    }
 
-	return 0;
+    return 0;
 }
